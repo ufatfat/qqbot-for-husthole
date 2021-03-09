48d0c270487f2830eb5c5e12bdb9a6d615213937
ufatfat <lzff@ufatfat.com>
2021-03-09 13:56:03 +0800

    0.0.2

--- event.go
+++ event.go
@@ -1,8 +1,10 @@
 package qqbot_for_husthole
 
+import "fmt"
+
 func (bot *QQBot) EventAddFriendRequest (userID, botID, encryptedEmail string) (err error) {
 	if _, err = bot.Db.Exec("insert into qq_bind (email, user_id, bot_id) values (?,?,?)", encryptedEmail, userID, botID); err != nil {
-		// error handle
+		fmt.Println("err: ", err.Error())
 		return
 	}
 	return
